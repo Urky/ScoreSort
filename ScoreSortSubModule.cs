f2291f4c969e9db7e33eafafcff6da0c18e537b2
Erik Vroon <erik_vroon@hotmail.com>
2020-04-28 23:43:20 +0200

    Add project files.

---
+++ ScoreSortSubModule.cs
@@ -0,0 +1,37 @@
+﻿using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.ViewModelCollection;
+
+namespace ScoreSort
+{
+    public class ScoreSortSubModule : MBSubModuleBase 
+    {
+        protected override void OnSubModuleLoad() 
+        {
+            
+        }
+    }
+
+    [HarmonyPatch(typeof(ScoreboardVM))]
+    [HarmonyPatch("UpdateQuitText")]
+    public static class UpdateQuitTextPatch
+    {
+        public static bool isOver { get; set; }
+
+        [HarmonyPostfix]
+        public static void PostFix(ScoreboardVM __instance)
+        {
+            if (__instance.IsOver)
+            {
+                foreach (var party in __instance.Attackers.Parties)
+                {
+                    party.Members = ScoreBoardHelper.GetOrderedScoreList(party);
+                }
+
+                foreach (var party in __instance.Defenders.Parties)
+                {
+                    party.Members = ScoreBoardHelper.GetOrderedScoreList(party);
+                }
+            }
+        }
+    }
+}
